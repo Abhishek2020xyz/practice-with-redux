9e161adad700c3a7990ad5015973d74dc9b5b756
Abhishek2020xyz <116792007+Abhishek2020xyz@users.noreply.github.com>
2023-03-09 20:25:54 +0530

    Add files via upload

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const createSlice ({
+const cartSlice = createSlice ({
     name: 'cart',
     initialState: {
     items: [],
@@ -10,9 +10,10 @@ const createSlice ({
         addItemToCart(state,action){
             const newItem = action.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
             if(!existingItem){
                 state.items.push({
-              itemId: newItem.id,
+               id: newItem.id,
                price: newItem.price,
                quantity:1,
                totalPrice:newItem.price,
@@ -23,7 +24,18 @@ const createSlice ({
                 existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
         },
-        removeItemFromCart() {},
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            const existingItem = state.items.find(item => item.id === id);
+            state.totalQuantity--;
+            if(existingItem.quantity === 1){
+                state.items = state.items.filter(item => item.id !== id);
+            }else{
+                existingItem.quantity--;
+            }
+        },
     },
 });
-export default createSlice;
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
